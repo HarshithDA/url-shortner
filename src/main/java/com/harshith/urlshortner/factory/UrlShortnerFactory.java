c3e8473dcb79bb9ba05d015a8d3dc51169b06499
HarshithDA <h.hegde7@gmail.com>
2020-11-05 18:40:35 +0530

    Wrapper exception handler for Application, Bug fix for fetching Original URL based on ID OR Unique Date

--- src/main/java/com/harshith/urlshortner/factory/UrlShortnerFactory.java
+++ src/main/java/com/harshith/urlshortner/factory/UrlShortnerFactory.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,84 +20,120 @@ import com.harshith.urlshortner.view.UrlShortenResponseView;
 @Service
 public class UrlShortnerFactory {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UrlShortnerFactory.class);
+
   @Autowired
   UrlShortnerService urlShortnerService;
 
   public String getShortenedUrl(String originalUrl) throws UrlShortnerServiceException {
+    try {
+      if (originalUrl == null || originalUrl.isBlank()) {
+        throw new UrlShortnerServiceException(
+            "Original URL cannot be null or empty, please provide a valid URL");
+      }
 
-    if (originalUrl == null || originalUrl.isBlank()) {
+      return urlShortnerService.getShortenedUrl(originalUrl);
+    } catch (UrlShortnerServiceException e) {
+      throw e;
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
       throw new UrlShortnerServiceException(
-          "Original URL cannot be null or empty, please provide a valid URL");
+          "Failed to Shorten URL! Unexpected Application Error Occured!", 101);
     }
-
-    return urlShortnerService.getShortenedUrl(originalUrl);
   }
 
 
   public String getShortenedUrlByUniqueDate(String originalUrl) throws UrlShortnerServiceException {
+    try {
+      if (originalUrl == null || originalUrl.isBlank()) {
+        throw new UrlShortnerServiceException(
+            "Original URL cannot be null or empty, please provide a valid URL");
+      }
 
-    if (originalUrl == null || originalUrl.isBlank()) {
+      return urlShortnerService.getShortenedUrlByUniqueDate(originalUrl);
+    } catch (UrlShortnerServiceException e) {
+      throw e;
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
       throw new UrlShortnerServiceException(
-          "Original URL cannot be null or empty, please provide a valid URL");
+          "Failed to Shorten URL by Date! Unexpected Application Error Occured!", 102);
     }
-
-    return urlShortnerService.getShortenedUrlByUniqueDate(originalUrl);
   }
 
 
   public UrlShortenResponseView getOriginalUrl(String shortenedUrl)
       throws UrlShortnerServiceException {
+    try {
+      if (shortenedUrl == null || shortenedUrl.isBlank()) {
+        throw new UrlShortnerServiceException(
+            "Shortened URL cannot be null or empty, please provide a valid Short URL");
+      }
 
-    if (shortenedUrl == null || shortenedUrl.isBlank()) {
+      return urlShortnerService.getOriginalUrl(shortenedUrl);
+    } catch (UrlShortnerServiceException e) {
+      throw e;
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
       throw new UrlShortnerServiceException(
-          "Shortened URL cannot be null or empty, please provide a valid Short URL");
+          "Failed to Fetch Original! Unexpected Application Error Occured!", 103);
     }
-
-    return urlShortnerService.getOriginalUrl(shortenedUrl);
   }
 
 
   public List<UrlShortenResponseView> getOriginalUrlList(int pageNumber, int pageSize,
-      SortByColumnEnum sortBy, SortOrderEnum sortByOrder) {
-    return urlShortnerService.getOriginalUrlList(pageNumber, pageSize, sortBy, sortByOrder);
+      SortByColumnEnum sortBy, SortOrderEnum sortByOrder) throws UrlShortnerServiceException {
+    try {
+      return urlShortnerService.getOriginalUrlList(pageNumber, pageSize, sortBy, sortByOrder);
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
+      throw new UrlShortnerServiceException(
+          "Failed to Fetch Original URLs List! Unexpected Application Error Occured!", 104);
+    }
   }
 
 
   public Response redirectToOriginalUrl(String shortenedUrl)
       throws UrlShortnerServiceException, URISyntaxException {
+    try {
+      if (shortenedUrl == null || shortenedUrl.isBlank()) {
+        throw new UrlShortnerServiceException(
+            "Shortened URL cannot be null or empty, please provide a valid Short URL");
+      }
 
-    if (shortenedUrl == null || shortenedUrl.isBlank()) {
+      String originalUrl = urlShortnerService.getOriginalUrl(shortenedUrl).getOriginalUrl();
+
+      URI uri = new URI(originalUrl);
+
+      return Response.status(Status.MOVED_PERMANENTLY).location(uri).build();
+      // return Response.temporaryRedirect(uri).build();
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
       throw new UrlShortnerServiceException(
-          "Shortened URL cannot be null or empty, please provide a valid Short URL");
+          "Failed to Redirect to Original URL! Unexpected Application Error Occured!", 105);
     }
-
-    String originalUrl = urlShortnerService.getOriginalUrl(shortenedUrl).getOriginalUrl();
-
-    URI uri = new URI(originalUrl);
-
-    return Response.status(Status.MOVED_PERMANENTLY).location(uri).build();
-    // return Response.temporaryRedirect(uri).build();
-
   }
 
 
   public ModelAndView redirectToOriginalUrlNew(String shortenedUrl,
       HttpServletResponse httpServletResponse)
       throws UrlShortnerServiceException, URISyntaxException {
+    try {
+      if (shortenedUrl == null || shortenedUrl.isBlank()) {
+        throw new UrlShortnerServiceException(
+            "Shortened URL cannot be null or empty, please provide a valid Short URL");
+      }
 
-    if (shortenedUrl == null || shortenedUrl.isBlank()) {
+      String originalUrl = urlShortnerService.getOriginalUrl(shortenedUrl).getOriginalUrl();
+
+      // httpServletResponse.setHeader("Location", originalUrl);
+      // httpServletResponse.setStatus(302);
+
+      return new ModelAndView("redirect:" + originalUrl);
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage(), e);
       throw new UrlShortnerServiceException(
-          "Shortened URL cannot be null or empty, please provide a valid Short URL");
+          "Failed to Redirect to Original URL! Unexpected Application Error Occured!", 106);
     }
-
-    String originalUrl = urlShortnerService.getOriginalUrl(shortenedUrl).getOriginalUrl();
-
-    // httpServletResponse.setHeader("Location", originalUrl);
-    // httpServletResponse.setStatus(302);
-
-    return new ModelAndView("redirect:" + originalUrl);
-
-
   }
 
 }
