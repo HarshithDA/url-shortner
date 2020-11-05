ea183e32f35c3fca5943d4ecabe96bc30c804dd0
HarshithDA <h.hegde7@gmail.com>
2020-11-06 00:52:24 +0530

    Mockito testing trials

--- src/test/java/com/harshith/urlshortner/UrlShortnerApplicationTests.java
+++ src/test/java/com/harshith/urlshortner/UrlShortnerApplicationTests.java
@@ -1,17 +1,24 @@
 package com.harshith.urlshortner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.time.Instant;
-import java.util.Date;
-import javax.annotation.Resource;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 import com.harshith.urlshortner.config.UrlShortnerJpaConfig;
-import com.harshith.urlshortner.entity.UrlShortenedEntity;
+import com.harshith.urlshortner.entity.UrlShortenedEntityMapper;
+import com.harshith.urlshortner.factory.UrlShortnerFactory;
 import com.harshith.urlshortner.service.UrlShortenedRepository;
+import com.harshith.urlshortner.service.UrlShortnerService;
+import com.harshith.urlshortner.service.UrlShortnerServiceException;
 
 @ContextConfiguration(classes = {UrlShortnerJpaConfig.class},
     loader = AnnotationConfigContextLoader.class)
@@ -19,17 +26,39 @@ import com.harshith.urlshortner.service.UrlShortenedRepository;
 @SpringBootTest
 class UrlShortnerApplicationTests {
 
-  @Resource
-  private UrlShortenedRepository urlShortenedRepository;
+  @InjectMocks
+  UrlShortnerFactory urlShortnerFactory;
+
+  @Mock
+  UrlShortnerService urlShortnerService;
+
+  @Mock
+  UrlShortenedRepository urlShortenedRepository;
+
+  @Mock
+  UrlShortenedEntityMapper urlShortenedEntityMapper;
+
+  @Before
+  public void init() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+
 
   @Test
-  public void givenStudent_whenSave_thenGetOk() {
-    UrlShortenedEntity entity = new UrlShortenedEntity(100L, "https://news.google.com/topstories",
-        new Date().getTime(), Instant.now());
-    Long urlId = urlShortenedRepository.save(entity).getUrlId();
+  public void shortenUrlTestNew() throws UrlShortnerServiceException {
+    String originalUrl = "https://news.google.com/topstories";
+    String shortUrl = urlShortnerFactory.getShortenedUrl(originalUrl);
+    verify(urlShortnerService, times(1)).getShortenedUrl(any(String.class));
 
-    UrlShortenedEntity entitySaved = urlShortenedRepository.findById(urlId).get();
-    assertEquals("https://news.google.com/topstories", entitySaved.getOriginalUrl());
+    assertNotNull(shortUrl);
   }
+
+  // @Test
+  // public void fetchOriginalUrlTest() throws UrlShortnerServiceException {
+  // String originalUrlFromMock = urlShortnerFactory.getOriginalUrl(shortUrl).getOriginalUrl();
+  // assertEquals(originalUrlFromMock, originalUrl);
+  // verify(urlShortnerService, times(1)).getOriginalUrl(any(String.class));
+  // }
 
 }
